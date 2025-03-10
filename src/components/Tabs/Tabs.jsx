185f6d88ec1ae38548f646f14b9107b0d4a16c18
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-03-10 17:23:21 +0200

    done1

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,18 @@
-export const Tabs = () => {};
+export const Tabs = ({ tab, select, setSelect }) => {
+  const handleClick = () => {
+    setSelect(tab);
+  };
+
+  return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+    <li
+      onClick={handleClick}
+      className={select === tab ? 'is-active' : ''}
+      data-cy="Tab"
+    >
+      <a href="#tab-1" data-cy="TabLink">
+        {tab.title}
+      </a>
+    </li>
+  );
+};
